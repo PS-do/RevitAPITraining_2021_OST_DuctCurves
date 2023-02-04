a296c9119191d1a7bfedff4e2a80731e10b3f26d
Сергей Плюхин <psg.pgs@mail.ru>
2023-02-04 15:39:13 +0300

    Добавьте файлы проекта.

---
+++ RevitAPITraining_2021_OST_DuctCurves/Main.cs
@@ -0,0 +1,34 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITraining_2021_OST_DuctCurves
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            ElementCategoryFilter ductCurvesCategoryFilter = new ElementCategoryFilter(BuiltInCategory.OST_DuctCurves);
+            ElementClassFilter ductCurvesInstancesFilter = new ElementClassFilter(typeof(FamilyInstance));  // выберет только те, которые являются экземплярами симейств(без типов)
+            LogicalAndFilter ductCurvesFilter = new LogicalAndFilter(ductCurvesCategoryFilter, ductCurvesInstancesFilter);
+
+            var ductCurves = new FilteredElementCollector(doc)
+                .WherePasses(ductCurvesFilter)
+                .Cast<FamilyInstance>()
+                .ToList();
+
+            TaskDialog.Show("DuctCurves info", ductCurves.Count.ToString());
+            return Result.Succeeded;
+        }
+    }
+}
